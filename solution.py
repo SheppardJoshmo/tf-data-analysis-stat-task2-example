4d167fdf1cd921d33032737b2c9256abc191d451
SheppardJoshmo <113803387+SheppardJoshmo@users.noreply.github.com>
2023-04-06 22:37:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,13 +7,14 @@ from scipy.stats import norm
 chat_id = 696934164 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    import pandas as pd
+    import numpy as np
     from scipy.stats import chi2
+    a = 1 - p
     n = len(x)
-    s2 = 0
-    for elem in x:
-        s2 = s2 + elem**2
-    c_1 = chi2.ppf(p/2, n)
-    c_2 = chi2.ppf(1 - p/2, n)
-    left = np.sqrt(s2 / (c_2 * 38))
-    right = np.sqrt(s2 / (c_1 * 38))
+    s2 = np.sum(x**2)
+    c_1 = chi2.ppf(a/2, 2 * n)
+    c_2 = chi2.ppf(1 - a/2, 2 * n)
+    left = np.sqrt(s2 / (c_2 * 34))
+    right = np.sqrt(s2 / (c_1 * 34))
     return left, right
